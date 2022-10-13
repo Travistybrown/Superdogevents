2267cb987c178e8e5842c6e6b4daa37b2d180577
Travistybrown <travistbrownsc@gmail.com>
2022-10-13 11:29:58 -0400

    sdfadfsadsfa

--- js/site.js
+++ js/site.js
@@ -1,17 +1,84 @@
-function displayMessage(){
-  
-    let msg =  document.getElementById("message").value;
+// this intial dataset for superdog. Is is an array of objects.
 
 
-    //alert(msg);
+const events = [
+  {
+    event: "ComicCon",
+    city: "New York",
+    state: "New York",
+    attendance: 240000,
+    date: "06/01/2017",
+  },
+  {
+    event: "ComicCon",
+    city: "New York",
+    state: "New York",
+    attendance: 250000,
+    date: "06/01/2018",
+  },
+  {
+    event: "ComicCon",
+    city: "New York",
+    state: "New York",
+    attendance: 257000,
+    date: "06/01/2019",
+  },
+  {
+    event: "ComicCon",
+    city: "San Diego",
+    state: "California",
+    attendance: 130000,
+    date: "06/01/2017",
+  },
+  {
+    event: "ComicCon",
+    city: "San Diego",
+    state: "California",
+    attendance: 140000,
+    date: "06/01/2018",
+  },
+  {
+    event: "ComicCon",
+    city: "San Diego",
+    state: "California",
+    attendance: 150000,
+    date: "06/01/2019",
+  },
+  {
+    event: "HeroesCon",
+    city: "Charlotte",
+    state: "North Carolina",
+    attendance: 40000,
+    date: "06/01/2017",
+  },
+  {
+    event: "HeroesCon",
+    city: "Charlotte",
+    state: "North Carolina",
+    attendance: 45000,
+    date: "06/01/2018",
+  },
+  {
+    event: "HeroesCon",
+    city: "Charlotte",
+    state: "North Carolina",
+    attendance: 50000,
+    date: "06/01/2019",
+  },
+];
 
-    swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg,
-        }
+//builds a list of specific cities
+function buildDropDown(){
 
-    )
+    let eventDD = document.getElementById("eventDropDown");
+    eventDD.innerHTML = "";
+
+    //grab my template from the template tag;
+    const template = document.getElementById("cityDD-template");
+
+    let curEvents = events;
+
+    //filter our array by distinct cities
+        // ["new york", "san diesgo", etc]
+    let distinctEvents = [...new Set(  curEvents.map((event) => event.city)  )   ];
 }
-
